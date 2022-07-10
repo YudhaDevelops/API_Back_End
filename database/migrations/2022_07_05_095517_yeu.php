87e2bdae49f376939ce3e977bea507b8ce78c8b6
boim212 <bimaboim7@gmail.com>
2022-07-10 20:19:00 +0700

    up di main juga

--- database/migrations/2022_07_05_095517_yeu.php
+++ database/migrations/2022_07_05_095517_yeu.php
@@ -13,104 +13,133 @@ return new class extends Migration
      */
     public function up()
     {
-        // Schema::create('users', function (Blueprint $table) {
-        //     $table->id('id');
-        //     $table->string('nama_lengkap');
-        //     $table->string('email')->unique();
-        //     $table->timestamp('email_verified_at')->nullable();
-        //     $table->string('password');
-        //     $table->string('role',7);
-        //     $table->rememberToken();
-        //     $table->timestamps();
-        // });
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('nama_lengkap', 100);
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->integer('role');
+        });
 
         Schema::create('verifikasi', function (Blueprint $table) {
             $table->unsignedBigInteger('id_admin');
             $table->unsignedBigInteger('id_relawan');
-            $table->integer('status_verifikasi')->default(0);
+            $table->integer('status_verifikasi');
+
             $table->foreign('id_admin') -> references('id') -> on('users');
             $table->foreign('id_relawan')-> references('id') -> on('users');
+
             $table->primary(['id_admin', 'id_relawan']);
-            $table->timestamps();
         });
 
-        // Schema::create('data_diri', function (Blueprint $table) {
-        //     // $table->id();
-        //     $table->unsignedBigInteger('id_user');
-        //     $table->enum('jenis_kelamin',['Laki-laki', 'Perempuan']);
-        //     $table->date('tanggal_lahir');
-        //     $table->string('no_telepon',20);
-        //     $table->text('alamat_ktp');
-        //     $table->text('alamat_domisili');
-        //     $table->enum('gol_darah',['A', 'B', 'AB', 'O']);
-        //     $table->enum('kondisi_disabilitas',['Tidak Ada','Disabilitas Fisik','Disabilitas Intelektual','Disabilitas Mental','Disabilitas Sensorik-Netra','Disabilitas Sensorik-Rungu','Disabilitas Sensorik-Wicara']);
-        //     $table->string('penyakit',255);
-        //     $table->enum('status_vaksin', ['Belum','Dosis 1','Dosis 2','Booster']);
-        //     $table->boolean('pernah_bekerja_sama');
-        //     $table->boolean('bpjs_kesehatan');
-        //     $table->boolean('bpjs_ketenagakerjaan');
-        //     $table->string('instansi', 255);
-        //     $table->string('file_pelatihan', 255);
-        //     $table->foreign('id_user') -> references('id') -> on('users');
-        //     $table->primary('id_user');
-        //     $table->timestamps();
-        // });
+        Schema::create('provinsi', function (Blueprint $table) {
+            $table->char('id',2);
+            $table->string('nama',255);
 
-        // Schema::create('keahlian', function (Blueprint $table) {
-        //     $table->id('id_keahlian');
-        //     $table->string('jenis_keahlian',25);
-        //     $table->timestamps();
-        // });
+            $table->primary('id');
+        });
 
-        // Schema::create('relawan_keahlian', function (Blueprint $table) {
-        //     $table->unsignedBigInteger('id_relawan');
-        //     $table->unsignedBigInteger('id_keahlian');
-        //     $table->foreign('id_relawan')->references('id')->on('users');
-        //     $table->foreign('id_keahlian')->references('id_keahlian')->on('keahlian');
-        //     $table->timestamps();
-        // });
+        Schema::create('kabupaten', function (Blueprint $table) {
+            $table->char('id',4);
+            $table->char('id_provinsi',2);
+            $table->string('nama',255);
 
-        // Schema::create('pengalaman', function (Blueprint $table) {
-        //     $table->id('id_pengalaman');
-        //     $table->string('nama_instansi',50);
-        //     $table->string('durasi_waktu',20);
-        //     $table->timestamps();
-        // });
+            $table->primary('id');
+            $table->foreign('id_provinsi')->references('id')->on('provinsi');
+        });
 
-        // Schema::create('relawan_pengalaman', function (Blueprint $table) {
-        //     $table->unsignedBigInteger('id_relawan');
-        //     $table->unsignedBigInteger('id_pengalaman');
-        //     $table->foreign('id_relawan')->references('id')->on('users');
-        //     $table->foreign('id_pengalaman')->references('id_pengalaman')->on('pengalaman');
-        //     $table->timestamps();
-        // });
+        Schema::create('kecamatan', function (Blueprint $table) {
+            $table->char('id',7);
+            $table->char('id_kabupaten',4);
+            $table->string('nama',255);
 
-        // Schema::create('project', function (Blueprint $table) {
-        //     $table->id('id_project');
-        //     $table->string('nama_project',50);
-        //     $table->string('foto',255);
-        //     $table->timestamps();
-        // });
+            $table->primary('id');
+            $table->foreign('id_kabupaten')->references('id')->on('kabupaten');
+        });
 
-        // Schema::create('relawan_project', function (Blueprint $table) {
-        //     $table->unsignedBigInteger('id_relawan');
-        //     $table->unsignedBigInteger('id_project');
-        //     $table->foreign('id_relawan')->references('id_user')->on('users');
-        //     $table->foreign('id_project')->references('id_project')->on('project');
-        //     $table->timestamps();
-        // });
+        Schema::create('kelurahan', function (Blueprint $table) {
+            $table->char('id',10);
+            $table->char('id_kecamatan',7);
+            $table->string('nama',255);
+
+            $table->primary('id');
+            $table->foreign('id_kecamatan')->references('id')->on('kecamatan');
+        });
+
+        Schema::create('data_diri', function (Blueprint $table) {
+            // $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->enum('jenis_kelamin',['Laki-laki', 'Perempuan']);
+            $table->date('tanggal_lahir');
+            $table->string('no_telepon',20);
+            $table->char('alamat_ktp', 10);
+            $table->string('kode_pos_ktp', 5);
+            $table->char('alamat_domisili', 10);
+            $table->string('kode_pos_domisili', 5);
+            $table->enum('gol_darah',['A', 'B', 'AB', 'O']);
+            $table->text('kondisi_disabilitas');
+            $table->string('penyakit',255);
+            $table->enum('status_vaksin', ['Belum','Dosis 1','Dosis 2','Booster']);
+            $table->boolean('pernah_bekerja_sama');
+            $table->boolean('bpjs_kesehatan');
+            $table->boolean('bpjs_ketenagakerjaan');
+            $table->string('instansi', 255);
+            $table->string('file_pelatihan', 255);
+            $table->boolean('ready')->default('0');
+
+            $table->foreign('id_user') -> references('id') -> on('users');
+            $table->foreign('alamat_ktp') -> references('id') -> on('kelurahan');
+            $table->foreign('alamat_domisili') -> references('id') -> on('kelurahan');
+
+            $table->primary('id_user');
+        });
+
+        Schema::create('keahlian', function (Blueprint $table) {
+            $table->id('id_keahlian');
+            $table->string('jenis_keahlian',25);
+        });
+
+        Schema::create('relawan_keahlian', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_relawan');
+            $table->unsignedBigInteger('id_keahlian');
+
+            $table->foreign('id_relawan')->references('id')->on('users');
+            $table->foreign('id_keahlian')->references('id_keahlian')->on('keahlian');
+        });
+
+        Schema::create('pengalaman', function (Blueprint $table) {
+            $table->id('id_pengalaman');
+            $table->string('nama_instansi',50);
+            $table->string('durasi_waktu',20);
+        });
+
+        Schema::create('relawan_pengalaman', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_relawan');
+            $table->unsignedBigInteger('id_pengalaman');
+
+            $table->foreign('id_relawan')->references('id')->on('users');
+            $table->foreign('id_pengalaman')->references('id_pengalaman')->on('pengalaman');
+        });
+
+        Schema::create('project', function (Blueprint $table) {
+            $table->id('id_project');
+            $table->string('nama_project',50);
+            $table->string('foto',255);
+        });
+
+        Schema::create('relawan_project', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_relawan');
+            $table->unsignedBigInteger('id_project');
+
+            $table->foreign('id_relawan')->references('id')->on('users');
+            $table->foreign('id_project')->references('id_project')->on('project');
+        });
+
+        
 
         
     }
 
-    /*
-
-    INSERT INTO `users` (`id`, `nama_lengkap`, `email`, `email_verified_at`, `password`, `type`, `remember_token`, `created_at`, `updated_at`) VALUES
-    (1, 'Kevin', 'coba0anonim1@gmail.com', '2022-07-07 09:43:14', '$2y$10$xLV2N7RjRWAaUVpjCy9DPOIeh8hpBH/OcIZ1TQZh/dsjgtIrIFTxK', 0, NULL, '2022-07-07 09:43:14', '2022-07-07 09:43:14'),
-    (2, 'Yudha', 'bimaboim8@gmail.com', '2022-07-07 09:43:31', '$2y$10$0pkmKIr4nQwEdytsii4Ue.Hea2sITNC5ZFtrc9tLANry9RcmxnJB2', 2, NULL, '2022-07-07 09:43:31', '2022-07-07 09:43:31'),
-    (3, 'Yoga', 'bimaboim12@gmail.com', NULL, '$2y$10$nTC87mRi/TapQaGHtzHJOuIP8j7mjvOHSc9D9EWYR/.SJbgWijnyO', 1, NULL, '2022-07-07 09:43:48', '2022-07-07 09:43:48');
-
-    */
     /**
      * Reverse the migrations.
      *
@@ -144,6 +173,18 @@ return new class extends Migration
         }
         if (Schema::hasTable('users')) {
             Schema::drop('users');
+        }
+        if (Schema::hasTable('kelurahan')) {
+            Schema::drop('kelurahan');
+        }
+        if (Schema::hasTable('kecamatan')) {
+            Schema::drop('kecamatan');
+        }
+        if (Schema::hasTable('kabupaten')) {
+            Schema::drop('kabupaten');
+        }
+        if (Schema::hasTable('provinsi')) {
+            Schema::drop('provinsi');
         }
     }
 };
